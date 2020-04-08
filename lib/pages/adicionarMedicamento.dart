a43d47bc09090f1216aced216aaa924ea598bb48
jotahd26 <jotahd26@gmail.com>
2020-04-08 12:14:27 +0100

    + AutocompleteTextField + Lista do tipo de medicamento

--- lib/pages/adicionarMedicamento.dart
+++ lib/pages/adicionarMedicamento.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:autocomplete_textfield/autocomplete_textfield.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
 
 class Addmedicamento extends StatefulWidget {
   @override
@@ -6,6 +11,37 @@ class Addmedicamento extends StatefulWidget {
 }
 
 class _State extends State<Addmedicamento> {
+  List<String> csv = new List();
+  GlobalKey<AutoCompleteTextFieldState<String>> key = new GlobalKey();
+
+@override
+void initState() {
+  // TODO: implement initState
+  super.initState();
+  loadCSV();
+  _dropDownMenuItems = getDropDownMenuItems();
+  _currentTipoMedicammento = _dropDownMenuItems[17].value; //seleciona a Unidade da lista
+}
+  List TipoMedicamentos =
+  ["Ampolas", "Aplicações", "Capsulas", "Colher de cha", "Colher de sopa","Gota","Grama","Inalações","Injeção","Miligrama","Mililitro","Pedaço","Penso","Pilula","Spray","Supositório","UI","Unidade"];
+
+  List<DropdownMenuItem<String>> _dropDownMenuItems;
+  String _currentTipoMedicammento;
+
+  List<DropdownMenuItem<String>> getDropDownMenuItems() {
+    List<DropdownMenuItem<String>> items = new List();
+    for (String tipoMedicammento in TipoMedicamentos) {
+      // here we are creating the drop down menu items, you can customize the item right here
+      // but I'll just use a simple text for this
+      items.add(new DropdownMenuItem(
+          value: tipoMedicammento,
+          child: new Text(tipoMedicammento)
+      ));
+    }
+    return items;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,40 +51,64 @@ class _State extends State<Addmedicamento> {
         backgroundColor: Colors.green.shade800,
       ),
       body: Column(
-        children: <Widget>[
+         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(left: 16, top: 16),
-            child: TextFormField(
-              validator: (val) =>
-              val.length > 3 ? null : 'Nome do medicamento não é valido',
+            padding: EdgeInsets.only(left: 16, top: 16,right: 16),
+            child: csv.length == 0 ? Text("A carregar dados..."):
+             SimpleAutoCompleteTextField(
+              key: key,
+              suggestions: csv,
               decoration: InputDecoration(
+                enabledBorder: new UnderlineInputBorder(
+                  borderSide: new BorderSide(color: Colors.green.shade800),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green.shade800),
+                ),
                 labelText: 'Nome do medicamento',
+                labelStyle: TextStyle(color: Colors.black),
                 hintText: 'Introduzir o nome do medicamento',
-                icon: Icon(Icons.person),
+                hintStyle: TextStyle(color: Colors.black),
+                icon: Icon(Icons.chevron_right,color: Colors.green.shade800),
+
                 isDense: true,
               ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(left: 16, right: 16, top: 16),
-            child: TextFormField(
-              validator: (val) =>
-              val.length > 3 ? null : 'Tipo de medicamento não é valido',
-              decoration: InputDecoration(
-                labelText: 'Tipo de medicamento',
-                hintText: 'Introduzir o tipo do medicamento',
-                icon: Icon(Icons.person),
-                isDense: true,
+           SizedBox(height: 10),  //Espaço entre o Nome do Medicamento e o Tipo do medicamento
+           new Row(
+             mainAxisAlignment: MainAxisAlignment.start,
+             children: <Widget>[
+
+              Padding(
+              padding: EdgeInsets.only(left: 16),
+               child: Icon(
+                 Icons.chevron_right,
+                 color: Colors.green.shade800,
+                 size: 20,
+               ),
               ),
-            ),
-          ),
+                  Padding(
+                  padding: EdgeInsets.only(left: 14,right: 25),
+                    child: new Text('Tipo de Medicamento',style: TextStyle(fontSize: 18)),
+                  ),
+              new DropdownButton(
+                      underline: Container(
+                        height: 2,
+                        color: Colors.green.shade800,
+                      ),
+                     value: _currentTipoMedicammento,
+                     items: _dropDownMenuItems,
+                     onChanged: changedDropDownItem,
+                   )
+             ],
+           ),
           FlatButton(
             color: Colors.blue,
             textColor: Colors.white,
             disabledColor: Colors.grey,
             disabledTextColor: Colors.black,
-            onPressed: () {
-              /*...*/
+            onPressed: (){
             },
             child: Text(
               "Guardar",
@@ -58,4 +118,25 @@ class _State extends State<Addmedicamento> {
       ),
     );
   }
+
+  Future<String> loadAsset(String path) async {
+    return await rootBundle.loadString(path);
+  }
+
+  void loadCSV() {
+    loadAsset('assets/lista.csv').then((String output) {
+
+      setState(() {
+        csv = output.split("\n");
+      });
+
+    });
+  }
+  void changedDropDownItem(String selectedTipoMedicamento) {
+    print("Selected city $selectedTipoMedicamento, we are going to refresh the UI");
+    setState(() {
+      _currentTipoMedicammento = selectedTipoMedicamento;
+    });
+  }
+
 }
