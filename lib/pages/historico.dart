324c3f56bdc4d8e2dd4870d0cdf0893c34c5bec0
monoa <joaoassuncao11@gmail.com>
2020-04-15 14:58:42 +0100

    implementação do calendário

--- lib/pages/historico.dart
+++ lib/pages/historico.dart
@@ -1,35 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 
 void main() => runApp(App());
 class App extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Historico',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
 
-        home: Historico(),
+      ),
+      home: Historico(),
     );
   }
 }
 class Historico extends StatefulWidget {
 
-  @override
-  _State createState() => _State();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _State extends State<Historico> {
+class _HomePageState extends State<Historico> {
+  CalendarController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = CalendarController();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Histórico"),
+        title: Text('Historico'),
         centerTitle: true,
         backgroundColor: Colors.green.shade800,
-
       ),
-      body: Center(
-        child: Text('Histórico',style: TextStyle(fontSize: 20)),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TableCalendar(
+              initialCalendarFormat: CalendarFormat.week,
+              calendarStyle: CalendarStyle(
+                  todayColor: Colors.redAccent,
+                  selectedColor: Colors.green,
+                  todayStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      color: Colors.white)),
+              headerStyle: HeaderStyle(
+                centerHeaderTitle: true,
+                formatButtonDecoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                formatButtonTextStyle: TextStyle(color: Colors.white),
+                formatButtonShowsNext: false,
+              ),
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              onDaySelected: (date, events) {
+                print(date.toIso8601String());
+              },
+              builders: CalendarBuilders(
+                selectedDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    )),
+                todayDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.redAccent,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    )),
+              ),
+              calendarController: _controller,
+            )
+          ],
+        ),
       ),
     );
   }
