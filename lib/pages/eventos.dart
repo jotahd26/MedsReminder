a3c04a5f8aee99a7db6a9504ea5238c3eeaf2945
jotahd26 <jotahd26@gmail.com>
2020-05-01 18:54:15 +0100

    +Pagina de carregamento eventos

--- lib/pages/eventos.dart
+++ lib/pages/eventos.dart
@@ -52,6 +52,7 @@ class _State extends State<Eventos> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
 
@@ -62,9 +63,9 @@ class _State extends State<Eventos> {
             centerTitle: true,
             backgroundColor: Colors.green.shade800,
           ),
-          body: Column(
+          body: carregado ? Column(
             children: <Widget>[
-              carregado ? Expanded(
+              Expanded(
                   child:
                   ListView.builder(
 
@@ -116,9 +117,57 @@ class _State extends State<Eventos> {
                         );
                       }
                   )
-              ): new Container(child: Text("A carregar dados..."),)
+              ),
             ],
-          )
+          ):  Container(
+            child: new Stack(
+              children: <Widget>[
+                new Container(
+                  alignment: AlignmentDirectional.center,
+                  decoration: new BoxDecoration(
+                    color: Colors.white70,
+                  ),
+                  child: new Container(
+                    decoration: new BoxDecoration(
+                        color: Colors.blue[200],
+                        borderRadius: new BorderRadius.circular(10.0)
+                    ),
+                    width: 300.0,
+                    height: 200.0,
+                    alignment: AlignmentDirectional.center,
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new Center(
+                          child: new SizedBox(
+                            height: 50.0,
+                            width: 50.0,
+                            child: new CircularProgressIndicator(
+                              value: null,
+                              strokeWidth: 7.0,
+                            ),
+                          ),
+                        ),
+                        new Container(
+                          margin: const EdgeInsets.only(top: 25.0),
+                          child: new Center(
+                            child: new Text(
+                              "A carregar dados...",
+                              style: new TextStyle(
+                                  fontSize: 23,
+                                  color: Colors.white
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         );
   }
   void insertlistmedicamento(int id) async{
